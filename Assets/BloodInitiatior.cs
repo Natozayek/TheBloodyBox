5c14e08ac79efc337fc38126e039315c3bcd19cb
natozayek <MiguelAntonio.PenalverZayek@georgebrown.ca>
2023-03-20 06:02:58 -0400

    Update
    Visual Desing Initial Screen

---
+++ Assets/BloodInitiatior.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodInitiatior : MonoBehaviour
+{
+    [SerializeField] ParticleSystem ParticleSystem;
+    [SerializeField] float durationTime;
+
+    float timer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+
+        if(timer > Random.RandomRange(1.5f,durationTime))
+        {
+            ParticleSystem.Play();
+            timer = 0;
+        }
+        
+    }
+}
--- Assets/[Scripts]/UI/Blink.cs
+++ Assets/[Scripts]/UI/Blink.cs
@@ -7,20 +7,46 @@ public class Blink : MonoBehaviour
 {
     [Range(0f, 1f)]
     [SerializeField] float fadeSpeed;
+    [SerializeField] GameObject Logo;
+    [SerializeField] GameObject Menu;
+    bool isMenu_Open;
+    [SerializeField]float i;
     private void Start()
     {
         StartCoroutine(FadeImage(true));
     }
+    private void Update()
+    {
+        if(isMenu_Open)
+        {
+            i = i - Time.deltaTime/10f;
+
+            if (i <= 0)
+            {
+                Menu.SetActive(true);
+                Logo.SetActive(false);
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                Logo.GetComponent<Image>().color = new Color(0.264f, 0.264f, 0.264f, i);
+                gameObject.GetComponent<Image>().color = new Color(0.264f, 0.264f, 0.264f, i);
+            }
+           
+
+
+        }
+    }
     IEnumerator FadeImage(bool fadeAway)
     {
         // fade from opaque to transparent
-        if (fadeAway)
+        if (fadeAway || isMenu_Open)
         {
             // loop over 1 second backwards
-            for (float i = 1f; i >= 0.5f; i -= Time.deltaTime)
+            for (float i = 0.9f; i >= 0.0f; i -= Time.deltaTime)
             {
                 // set color with i as alpha
-                gameObject.GetComponent<Image>().color = new Color(1, 1, 1, i);
+                gameObject.GetComponent<Image>().color= new Color(0.264f, 0.264f, 0.264f, i);
                 yield return null;
             }
             fadeAway = false;
@@ -29,14 +55,19 @@ public class Blink : MonoBehaviour
         if(!fadeAway) 
         {
             // loop over 1 second
-            for (float i = 0.5f; i <= 1f; i += Time.deltaTime)
+            for (float i = 0.0f; i <= 1f; i += Time.deltaTime)
             {
                 // set color with i as alpha
-                gameObject.GetComponent<Image>().color = new Color(1, 1, 1, i);
+                gameObject.GetComponent<Image>().color = new Color(0.264f, 0.264f, 0.264f, i);
                 yield return null;
             }
         }
         yield return new WaitForSeconds(fadeSpeed);
         StartCoroutine(FadeImage(true));
+    }
+
+    public void ActivateMenu()
+    {
+        isMenu_Open = true;
     }
 }
